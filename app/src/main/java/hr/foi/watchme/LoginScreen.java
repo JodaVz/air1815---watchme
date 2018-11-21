417aa4247a1ee8027bb61418196bf8a8ecd493cd
David Belcar <dbelcar@foi.hr>
2018-11-21 17:14:18 +0100

    Set LoginScreen as main starting activity, added button click events on LoginScreen for changing activities, added Registration button

--- app/src/main/java/hr/foi/watchme/LoginScreen.java
+++ app/src/main/java/hr/foi/watchme/LoginScreen.java
@@ -1,13 +1,35 @@
 package hr.foi.watchme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginScreen extends AppCompatActivity {
+public class LoginScreen extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        Button buttonLogin = findViewById(R.id.confirm_login);
+        Button buttonRegistration = findViewById(R.id.confirm_registration);
+        buttonLogin.setOnClickListener(this);
+        buttonRegistration.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View viewLogin) {
+        switch (viewLogin.getId()){
+            case R.id.confirm_login:
+                Intent intentLogin = new Intent(this, MainActivity.class);
+                startActivity(intentLogin);
+                break;
+            case R.id.confirm_registration:
+                break;
+            default:
+                break;
+        }
     }
 }
