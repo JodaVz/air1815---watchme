7450b1a9f2ac6b4309fd4527f1311586465e4912
Filip Gatarić <filgatari@foi.hr>
2018-11-21 16:28:11 +0100

    -Added functionality of opening fragments by clicking on icons in drawer -When drawer is open, "Back" closes the drawer, not the app

--- app/src/main/java/hr/foi/watchme/MainActivity.java
+++ app/src/main/java/hr/foi/watchme/MainActivity.java
@@ -1,16 +1,19 @@
 package hr.foi.watchme;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
@@ -23,11 +26,41 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        //It will show movie fragment on create
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new MovieFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_tv_series);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_movie:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MovieFragment()).commit();
+                break;
+            case R.id.nav_tv_series:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new TvSeriesFragment()).commit();
+                break;
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MovieFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return false;
     }
 
     @Override
