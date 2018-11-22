5a6178852ad694d8b1d5e29df1d3bbbcc2d8ae43
David Belcar <dbelcar@foi.hr>
2018-11-23 00:39:23 +0100

    -Started implementing video player -Added new module :player -Created new fullscreen activity for the player

--- app/src/main/java/hr/foi/watchme/LoginScreen.java
+++ app/src/main/java/hr/foi/watchme/LoginScreen.java
@@ -6,30 +6,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.player.PlayerActivity;
+
 public class LoginScreen extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie_details);
+        setContentView(R.layout.activity_login_screen);
 
-        //Button buttonLogin = findViewById(R.id.confirm_login);
-        //Button buttonRegistration = findViewById(R.id.confirm_registration);
-        //buttonLogin.setOnClickListener(this);
-        //buttonRegistration.setOnClickListener(this);
+        Button buttonLogin = findViewById(R.id.confirm_login);
+        Button buttonRegistration = findViewById(R.id.confirm_registration);
+        buttonLogin.setOnClickListener(this);
+        buttonRegistration.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View viewLogin) {
-        /*switch (viewLogin.getId()){
+        switch (viewLogin.getId()){
             case R.id.confirm_login:
                 Intent intentLogin = new Intent(this, MainActivity.class);
                 startActivity(intentLogin);
                 break;
             case R.id.confirm_registration:
+                Intent intentTestPlayer = new Intent(this, PlayerActivity.class);
+                startActivity(intentTestPlayer);
                 break;
             default:
                 break;
-        }*/
+        }
     }
 }
---
+++ player/src/main/java/com/example/player/PlayerActivity.java
@@ -0,0 +1,41 @@
+package com.example.player;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    private void showSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
