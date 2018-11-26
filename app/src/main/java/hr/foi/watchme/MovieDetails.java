d97128fa8b5b9e4c135a49cfbd5f27cd1931114c
Filip Gatarić <filgatari@foi.hr>
2018-11-26 16:27:22 +0100

    -Added button for expanding text -Created MovieDetails. java -Created icons for show more/less text

---
+++ app/src/main/java/hr/foi/watchme/MovieDetails.java
@@ -0,0 +1,14 @@
+package hr.foi.watchme;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MovieDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+
+    }
+}
