b7398c20df6882798ef13bf5780917ee52bc30ae
Filip Gatarić <filip.gataric@gmail.com>
2019-01-14 13:52:31 +0100

    -Added getAllMoviesByCategories() method of type List<MovieCategory> -Created class MovieCategory.java

---
+++ app/src/main/java/hr/foi/watchme/MovieCategory.java
@@ -0,0 +1,34 @@
+package hr.foi.watchme;
+
+import java.util.List;
+
+import hr.foi.watchme.WebServiceApi.POJO.Movie;
+
+//TODO refaktorirati ovu klasu tako da se stavi u pravi paket
+public class MovieCategory{
+    private String name;
+    private List<Movie> movies;
+
+    public MovieCategory(String catName, List<Movie> movies){
+        this.name = catName;
+        this.movies = movies;
+    }
+
+
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- app/src/main/java/hr/foi/watchme/MoviesInterface.java
+++ app/src/main/java/hr/foi/watchme/MoviesInterface.java
@@ -6,4 +6,5 @@ import hr.foi.watchme.WebServiceApi.POJO.Movie;
 
 public interface MoviesInterface {
     public List<Movie> getMovieList();
+    public List<MovieCategory> getAllMoviesByCategories();
 }
