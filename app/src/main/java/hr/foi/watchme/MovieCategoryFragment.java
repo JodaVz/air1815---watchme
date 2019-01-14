275080a2cc241e34c7052a745d9ab9cf265d9fb2
Filip Gatarić <filip.gataric@gmail.com>
2019-01-14 14:26:21 +0100

    -Fragment fetches category name and list of movies in category

--- app/src/main/java/hr/foi/watchme/MovieCategoryFragment.java
+++ app/src/main/java/hr/foi/watchme/MovieCategoryFragment.java
@@ -8,7 +8,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import java.io.Serializable;
 import java.util.List;
 
 import hr.foi.watchme.WebServiceApi.CategoryAdapter;
@@ -16,20 +18,39 @@ import hr.foi.watchme.WebServiceApi.POJO.Movie;
 
 public class MovieCategoryFragment extends Fragment {
 
+    private String catName;
     private List<Movie> filmovi;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View viewMain = inflater.inflate(R.layout.fragment_category, container, false);
+
+        catName = getArguments().getString("NAME");
+        TextView tvCatName = viewMain.findViewById(R.id.output_category_name);
+        tvCatName.setText(catName);
+
+        filmovi = (List<Movie>) getArguments().getSerializable("MOVIES");
+
         return viewMain;
+    }
+
+    public static MovieCategoryFragment newInstance(String categoryName, List<Movie> movies) {
+        MovieCategoryFragment fragment = new MovieCategoryFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("NAME", categoryName);
+        bundle.putSerializable("MOVIES", (Serializable) movies);
+        fragment.setArguments(bundle);
+
+        return fragment;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         //TODO u neku metodu (On ActivityCreateed ili sličnu - pogledati živtni ciklus fragmenta) postaviti kod koji instancira adapter i proslijeđuje mu listu filmova RIJEŠENO!
-        //TODO ovaj fragment mora kroz konstruktor ili metodu getInstance primiti listu filmova i naziv kategorije.
+        //TODO ovaj fragment mora kroz konstruktor ili metodu getInstance primiti listu filmova i naziv kategorije. RIJEŠENO!
+
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
 
         RecyclerView myList = getView().findViewById(R.id.categoryRecycler);
