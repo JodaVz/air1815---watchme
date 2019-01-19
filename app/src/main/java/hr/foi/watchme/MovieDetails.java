fc59abdb85b974e93bf65c5e9b164342fdc38e25
Filip Gatarić <filip.gataric@gmail.com>
2019-01-19 12:25:20 +0100

    -MovieDetails switched to Fragment

--- app/src/main/java/hr/foi/watchme/MovieDetails.java
+++ app/src/main/java/hr/foi/watchme/MovieDetails.java
@@ -2,14 +2,35 @@ package hr.foi.watchme;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.player.PlayerActivity;
 
-public class MovieDetails extends AppCompatActivity implements View.OnClickListener {
+public class MovieDetails extends Fragment implements View.OnClickListener {
 
+
+    private MoviesInterface mListenerActivity;
+    LinearLayout catContainer;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View viewMain = inflater.inflate(R.layout.activity_movie_details, container, false);
+        catContainer = viewMain.findViewById(R.id.fragment_container);
+
+        ImageView moviePosterFront = getView().findViewById(R.id.iv_movie_poster_front);
+        moviePosterFront.setOnClickListener(this);
+
+        return viewMain;
+    }
+/*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +39,12 @@ public class MovieDetails extends AppCompatActivity implements View.OnClickListe
         ImageView moviePosterFront = findViewById(R.id.iv_movie_poster_front);
         moviePosterFront.setOnClickListener(this);
     }
-
+*/
     @Override
     public void onClick(View viewMovieDetails) {
         switch (viewMovieDetails.getId()){
             case R.id.iv_movie_poster_front:
-                Intent intentPlayerMovieDetails = new Intent(this, PlayerActivity.class);
+                Intent intentPlayerMovieDetails = new Intent(getActivity(), PlayerActivity.class);
                 startActivity(intentPlayerMovieDetails);
                 break;
             default:
