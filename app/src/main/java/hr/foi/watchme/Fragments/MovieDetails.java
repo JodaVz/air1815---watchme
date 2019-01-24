aab3cb6bb15911edded6d825293fdb81aa4f9fed
Filip Gatarić <filip.gataric@gmail.com>
2019-01-24 23:00:07 +0100

    -Setting text, and image from movie to TextView, ImageView -Implemented ShowMore/ShowLess functionality for about movie text

--- app/src/main/java/hr/foi/watchme/Fragments/MovieDetails.java
+++ app/src/main/java/hr/foi/watchme/Fragments/MovieDetails.java
@@ -1,4 +1,4 @@
-package hr.foi.watchme.POJO;
+package hr.foi.watchme.Fragments;
 
 import android.content.Context;
 import android.content.Intent;
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -16,12 +17,16 @@ import com.example.player.PlayerActivity;
 import com.squareup.picasso.Picasso;
 
 import hr.foi.watchme.Interfaces.MovieDetailsInterface;
+import hr.foi.watchme.Interfaces.MovieIdInterface;
+import hr.foi.watchme.POJO.Movie;
 import hr.foi.watchme.R;
 
 public class MovieDetails extends Fragment implements View.OnClickListener {
 
     private Movie movie;
+    private int movieId;
     private MovieDetailsInterface mListenerActivity;
+    private MovieIdInterface miListenerActivity;
     LinearLayout catContainer;
 
     ImageView moviePosterBack;
@@ -30,22 +35,53 @@ public class MovieDetails extends Fragment implements View.OnClickListener {
     TextView movieYear;
     TextView movieRating;
     TextView movieAbout;
+    Button button;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        FetchMovie();
-
         View viewMain = inflater.inflate(R.layout.activity_movie_details, container, false);
         catContainer = viewMain.findViewById(R.id.fragment_container);
 
+
+/*
         ImageView moviePosterFront = getView().findViewById(R.id.iv_movie_poster_front);
         moviePosterFront.setOnClickListener(this);
 
+*/
         return viewMain;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        movieTitle = getView().findViewById(R.id.output_movie_details_title);
+        moviePosterFront = getView().findViewById(R.id.iv_movie_poster_front);
+        moviePosterBack = getView().findViewById(R.id.iv_movie_poster_back);
+        movieAbout = getView().findViewById(R.id.output_movie_details_about);
+        movieRating = getView().findViewById(R.id.output_movie_details_rating);
+        movieYear = getView().findViewById(R.id.output_movie_details_year);
+        FetchMovie();
+
+        button = getView().findViewById(R.id.action_movie_details_show_more);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (button.getBackground().getConstantState()==getResources().getDrawable(R.drawable.ic_show_more).getConstantState()) {
+                    movieAbout.setMaxLines(Integer.MAX_VALUE);
+                    button.setBackground(getResources().getDrawable(R.drawable.ic_show_less));
+                } else {
+                    movieAbout.setMaxLines(2);//your TextView
+                    button.setBackground(getResources().getDrawable(R.drawable.ic_show_more));
+                }
+            }
+        });
+    }
+
     public void FetchMovie() {
+        movieId = miListenerActivity.getMovieId();
         movie = mListenerActivity.getMovieById();
 
         bind(movie);
@@ -54,8 +90,14 @@ public class MovieDetails extends Fragment implements View.OnClickListener {
     public void bind(Movie m) {
         movieTitle.setText(m.getName());
         movieYear.setText(m.getReleaseDate());
-        movieAbout.setText(m.getEpisode());
-        movieRating.setText(m.getFeedback());
+        movieAbout.setText("Lorem Ipsum is simply dummy text of the printing and typesetting industry." +
+                " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, " +
+                "when an unknown printer took a galley of type and scrambled it to make a type specimen book. " +
+                "It has survived not only five centuries, but also the leap into electronic typesetting, " +
+                "remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages," +
+                " and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+        movieRating.setText("" + m.getFeedback());
+
         Picasso.get()
                 .load(m.getCoverPhoto())
                 .resize(120, 160)
@@ -82,10 +124,10 @@ public class MovieDetails extends Fragment implements View.OnClickListener {
         super.onAttach(context);
 
         try {
+            miListenerActivity = (MovieIdInterface) getActivity();
             mListenerActivity = (MovieDetailsInterface) getActivity();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
-
 }
