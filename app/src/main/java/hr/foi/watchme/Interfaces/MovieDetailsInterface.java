447d15f423372a82ade487097f7bed28ff70035e
Filip Gatarić <filip1310@hotmail.com>
2019-01-31 10:59:58 +0100

    -Commented interfaces

--- app/src/main/java/hr/foi/watchme/Interfaces/MovieDetailsInterface.java
+++ app/src/main/java/hr/foi/watchme/Interfaces/MovieDetailsInterface.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import hr.foi.watchme.POJO.Movie;
 
 public interface MovieDetailsInterface {
+    //Used to get name and list of movies that are in selected category
     void categoryClicked(String catName, ArrayList<Movie> movies);
+    //Used to get data from selected movie
     void movieClicked(Movie m);
 }
--- app/src/main/java/hr/foi/watchme/Interfaces/MoviesInterface.java
+++ app/src/main/java/hr/foi/watchme/Interfaces/MoviesInterface.java
@@ -6,6 +6,8 @@ import hr.foi.watchme.POJO.MovieCategory;
 import hr.foi.watchme.POJO.Movie;
 
 public interface MoviesInterface {
+    //Used to get list of all movies
     public List<Movie> getMovieList();
+    //Used to get list of movies by category
     public List<MovieCategory> getAllMoviesByCategories();
 }
